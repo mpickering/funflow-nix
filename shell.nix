62215e6c35aad2b1c5d8ac17bbce44c35f08bce7
Matthew Pickering <matthewtpickering@gmail.com>
2018-11-16 13:15:26 +0000

    ghc843 -> ghc844

--- shell.nix
+++ shell.nix
@@ -3,4 +3,4 @@ let
 in
   np.mkShell { buildInputs = [ np.haskell.packages.ghc822.ghc
   np.haskell.packages.ghc861.cabal-install
-  np.haskell.packages.ghc843.haskell-ci ]; }
+  np.haskell.packages.ghc844.haskell-ci ]; }
