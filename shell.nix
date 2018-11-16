5997bdf1d0806ed94f6373ec3503b038c9d0e6f7
Matthew Pickering <matthewtpickering@gmail.com>
2018-11-16 13:12:36 +0000

    Remove simple-server from shell.nix

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ let
   np = import <nixpkgs> {};
 in
   np.mkShell { buildInputs = [ np.haskell.packages.ghc822.ghc
-  np.haskell.packages.ghc861.cabal-install np.simple-server
+  np.haskell.packages.ghc861.cabal-install
   np.haskell.packages.ghc843.haskell-ci ]; }
